ed75cfec6b9af95d7bec3189cb6611977dc0ff47
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-02-23 18:36:49 +0500

    x18

--- basic_func18.py
+++ basic_func18.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, ceil
 def main(a):
     '''Assign the value pi to the parametr "a". Round the result to 2 decimal places and return it.
     
@@ -8,6 +8,6 @@ def main(a):
     Returns:
         float: the result.
     '''
-    return round(a, 2)
+    return ceil(a)
 
 print(main(pi))
